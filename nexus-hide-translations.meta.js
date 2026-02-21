5b91fe547de4c53d55ddbe941442b2c29df47d76
r3nn <scopolamina087@hotmail.com>
2026-02-20 22:16:06 -0300

    added meta

---
+++ nexus-hide-translations.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        Nexus Mods - Hide Translations from Requirements
+// @namespace   Violentmonkey Scripts
+// @icon        https://www.google.com/s2/favicons?sz=64&domain=nexusmods.com
+// @match       https://www.nexusmods.com/*/mods/*
+// @grant       none
+// @version     0.5
+// @author      r3nn431
+// @description Hides mods that appear in the requirements tab if they are already listed as translations.
+// @run-at      document-idle
+// @downloadURL https://raw.githubusercontent.com/r3nn431/Scripts/main/nexus-hide-translations.user.js
+// @updateURL   https://raw.githubusercontent.com/r3nn431/Scripts/main/nexus-hide-translations.meta.js
+// ==/UserScript==
