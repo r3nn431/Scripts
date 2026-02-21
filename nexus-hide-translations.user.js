8fc53e9037c1d77545b5fb8359962fa028d7c43e
r3nn <scopolamina087@hotmail.com>
2026-02-20 22:16:51 -0300

    update URL to meta

--- nexus-hide-translations.user.js
+++ nexus-hide-translations.user.js
@@ -4,12 +4,12 @@
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=nexusmods.com
 // @match       https://www.nexusmods.com/*/mods/*
 // @grant       none
-// @version     0.4
+// @version     0.5
 // @author      r3nn431
 // @description Hides mods that appear in the requirements tab if they are already listed as translations.
 // @run-at      document-idle
 // @downloadURL https://raw.githubusercontent.com/r3nn431/Scripts/main/nexus-hide-translations.user.js
-// @updateURL   https://raw.githubusercontent.com/r3nn431/Scripts/main/nexus-hide-translations.user.js
+// @updateURL   https://raw.githubusercontent.com/r3nn431/Scripts/main/nexus-hide-translations.meta.js
 // ==/UserScript==
 
 (function() {
