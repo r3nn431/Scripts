e309924c9e1b92f4438c6a86209585565c7a2d73
r3nn <scopolamina087@hotmail.com>
2026-02-20 23:36:51 -0300

    Update version number to 1.0

--- nexus-hide-translations.meta.js
+++ nexus-hide-translations.meta.js
@@ -4,7 +4,7 @@
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=nexusmods.com
 // @match       https://www.nexusmods.com/*/mods/*
 // @grant       none
-// @version     0.5
+// @version     1.0
 // @author      r3nn431
 // @description Hides mods that appear in the requirements tab if they are already listed as translations.
 // @run-at      document-idle
